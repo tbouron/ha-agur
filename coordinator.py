d34eb1aba8e4193e01f36aec6e111ff7c000fd68
Thomas Bouron <thomas.bouron@cloudsoftcorp.com>
2023-12-17 18:26:27 +0100

    Add support to import historical statistics to HA

--- coordinator.py
+++ coordinator.py
@@ -4,6 +4,10 @@ import asyncio
 import logging
 from datetime import timedelta, datetime
 
+from homeassistant.components.recorder import get_instance
+from homeassistant.components.recorder.models import StatisticData, StatisticMetaData
+from homeassistant.components.recorder.statistics import get_last_statistics, async_add_external_statistics
+from homeassistant.const import UnitOfVolume
 from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import ConfigEntryAuthFailed
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
@@ -78,6 +82,10 @@ class AgurDataUpdateCoordinator(DataUpdateCoordinator[dict[str, AgurDataUpdateCo
                 )
                 data[contract_id] = AgurDataUpdateCoordinatorData(data_points=results[0], contract=results[1])
 
+                # TODO: Make this configurable with a config_entry.option
+                # TODO: This should not be within the coordinator. Possibly in the __init__.py or in sensors.py
+                await self._insert_statistics(coordinator_data=data[contract_id])
+
             return data
 
         except HTTPError as exception:
@@ -105,3 +113,62 @@ class AgurDataUpdateCoordinator(DataUpdateCoordinator[dict[str, AgurDataUpdateCo
     async def _async_get_contract(self, contract_id) -> AgurContract:
         client = AgurClient(session_token=self.session_token, auth_token=self.auth_token)
         return await self.hass.async_add_executor_job(client.get_contract, contract_id)
+
+    async def _insert_statistics(self, coordinator_data: AgurDataUpdateCoordinatorData):
+        # I've decided to insert statics without a backing of a sensor. Few integrations in core use the same principal
+        # For example:
+        # - https://github.com/home-assistant/core/blob/dev/homeassistant/components/tibber/sensor.py#L594-L690
+        # - https://github.com/home-assistant/core/blob/dev/homeassistant/components/opower/coordinator.py#L89-L188
+        #
+        # Another solution would be to use a sensor for this using the following library:
+        # https://github.com/ldotlopez/ha-historical-sensor
+
+        statistic_id = f"{DOMAIN}:water_consumption_{coordinator_data.contract.id}"
+        daily_indexes_data = coordinator_data.data_points
+        last_stats = await get_instance(self.hass).async_add_executor_job(
+            get_last_statistics,
+            self.hass,
+            1,
+            statistic_id,
+            True,
+            set("sum")
+        )
+
+        if not last_stats:
+            # If the statistic does not exist, it means we are importing it for the first time, i.e. we import the
+            # entire set of `daily_index_data`
+            min_start = None
+        else:
+            # Otherwise, we want to reimport the last 30 days of data, just in case there are some corrections that
+            # need to be done
+            # `daily_index_data[0]` is the most recent data point, so we get that date and subtract 30 days.
+            min_start = daily_indexes_data[0].date - timedelta(days=30)
+
+        statistics = []
+
+        for index, daily_index_data in enumerate(list(reversed(daily_indexes_data))):
+            _start = daily_index_data.date
+            if min_start is not None and _start <= min_start:
+                continue
+
+            _state = daily_index_data.value - daily_indexes_data[index - 1].value if index > 0 else 0
+            _sum = daily_index_data.value
+
+            statistics.append(
+                StatisticData(
+                    start=_start,
+                    state=_state,
+                    sum=_sum,
+                )
+            )
+
+        unit = UnitOfVolume.LITERS
+        metadata = StatisticMetaData(
+            has_mean=False,
+            has_sum=True,
+            name=f"Water consumption",
+            source=DOMAIN,
+            statistic_id=statistic_id,
+            unit_of_measurement=unit,
+        )
+        async_add_external_statistics(self.hass, metadata, statistics)
